d94d075a4e4797986472cc3ce7264aebb8e85576
Duy Thanh Tran <bradduy@icloud.com>
2021-02-07 21:15:12 +0900

    add more fucntionalities

--- web/fastAPI.py
+++ web/fastAPI.py
@@ -1,20 +1,40 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-import json
+from typing import Optional
 
 app = FastAPI()
 
+
 class Dog(BaseModel):
-    id: float
+    id: int
     name: str
     status: str = 'good'
+
 
 @app.get("/")
 async def getAll():
     return JSONResponse({"Notification": "Empty data, so pls come back later"})
 
+
+@app.get("/getDogById/{id}")
+async def getDogById(id: int):
+    return JSONResponse({"Result": "You are finding the dog number " + str(id)})
+
+
+@app.get("/getDogByIdOrName")
+async def getADog(id: int, name: Optional[str]):
+    if name == None or name == "":
+        return JSONResponse({"Result": "You are finding the dog number " + str(id)})
+
+    return JSONResponse(
+        {
+            "firsResult": "You are finding the dog number " + str(id),
+            "secondResult": "You are finding the dog name " + str(name),
+        }
+    )
+
+
 @app.post("/detectDog")
 async def detectDog(dog: Dog):
     return JSONResponse({str(dog.name): "is " + str(dog.status)})
-
